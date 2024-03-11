6333e9292b9a95cf8d9781b1f6832d3f7cea2126
FariaMobeen <113133002+FariaMobeen@users.noreply.github.com>
2024-03-11 16:58:20 -0600

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,7 +12,7 @@ function Experience() {
   return (
     <div id="experience" className="flex flex-col md:flex-row mt-5">
       <div className="w-full md:w-1/3 flex items-center justify-center p-4">
-        <h2 className="text-3xl font-bold text-center text-textGreen">Experience</h2>
+        <h2 className="text-3xl font-bold text-center text-textGreen" style ={{color: '#306dbc', fontWeight:'700'}}>Experience</h2>
       </div>
       <div className="w-full md:w-2/3 items-center">
         <VerticalTimeline lineColor="#ccd6f6">
